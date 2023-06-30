ca5d6e8d080164c7038dec3ee3fcc6b5e6a95536
Olga-Olga <olga_l@ukr.net>
2023-06-30 18:57:53 +0300

    123

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+player.play(); // Воспроизведение
+player.pause(); // Пауза
+player.setCurrentTime(30); // Установка времени воспроизведения
+player.setVolume(0.5); 
+    player.on('play', function() {
+        console.log('Воспроизведение началось');
+    });
+
+    player.on('pause', function() {
+  console.log('Воспроизведение приостановлено');
+    });
+
+player.on('timeupdate', function () {
+    console.log('timeupdate', data.seconds);
+    });
+
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+    const onPlay = function(data) {
+    // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
