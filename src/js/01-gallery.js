d98b2095c228ac4fed29ae4fe1dc8a217c90bbff
Olga-Olga <olga_l@ukr.net>
2023-06-30 11:36:46 +0300

    123

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,42 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+// import SimpleLightbox from "../../node_modules/simplelightbox/dist/simple-lightbox.esm"
+// import SimpleLightbox from "simplelightbox";
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+
+const galeryElements = galleryItems.map( ({preview,original,description}) => `<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+   </a>
+</li>`).join("")
+const ulEl = document.querySelector(".gallery")
+ulEl.insertAdjacentHTML("afterbegin", galeryElements)
+
+const br = new SimpleLightbox(".gallery a", {
+   captionsData: "alt",
+   captionDelay: 250,
+   doubleTapZoom: 2,
+   scrollZoom: false
+})
+
+
+// br.on('next.simplelightbox', function () {
+//    console.log("Hello");
+//    br.close()
+// });
+
+// const lightbox = new SimpleLightbox('.gallery a');
+// let currentIndex = 0;
+// let intervalId;
+// function autoNextImage(delay) {
+//   intervalId = setInterval(() => {
+//     lightbox.close();
+//     currentIndex = (currentIndex + 1) % lightbox.elements.length;
+//     const nextElement = lightbox.elements[currentIndex];
+//     lightbox.open(nextElement);
+//   }, delay);
+// }
+// autoNextImage(5000);
